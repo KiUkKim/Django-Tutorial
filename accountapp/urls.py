3f244627ae57c17dd1881e64acadc48d7f0a5b1c
KiUkKim <zidh1@korea.ac.kr>
2022-01-23 14:35:24 +0900

    django course 22

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,9 +1,17 @@
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
-from accountapp.views import hello_world
+from accountapp.views import hello_world, AccountCreateView
 
 app_name = "accountapp"
 
 urlpatterns = [
-    path('hello_world/', hello_world, name='hello_world')
+    path('hello_world/', hello_world, name='hello_world'),
+
+    path('login', LoginView.as_view(template_name='accountapp/login.html'), name='login'),
+    path('logout', LogoutView.as_view(), name='logout'),
+
+
+    # class view에서는 as_view()를 사용해서 함수형 처럼 받아준다.
+    path('create/', AccountCreateView.as_view(), name='create'),
 ]
--- accountapp/views.py
+++ accountapp/views.py
@@ -1,10 +1,12 @@
-
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 
 # Create your views here.
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView
 
 from accountapp.models import HelloWorld
 
@@ -24,3 +26,14 @@ def hello_world(request):
         hello_world_list = HelloWorld.objects.all()
         return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
 
+
+class AccountCreateView(CreateView):
+    # User라는 장고에서 기본적으로 사용하는 model사용
+    model = User
+    # form 지정
+    form_class = UserCreationForm
+    # 연결 성공시 이동하는 페이지 지정
+    # reverse는 그대로 class에서 사용할 수 없기때문에, class형 view에서는 reverse_lazy
+    # reverse는 function형 view에서 사용
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'accountapp/create.html'
