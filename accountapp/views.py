f824e9a1280eda68072574abef0c73e58414ddfd
KiUkKim <zidh1@korea.ac.kr>
2022-01-23 23:31:41 +0900

    django 26 course commit

--- accountapp/views.py
+++ accountapp/views.py
@@ -6,7 +6,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse, reverse_lazy
-from django.views.generic import CreateView, DetailView, UpdateView
+from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 
 from accountapp.forms import AccountUpdateForm
 from accountapp.models import HelloWorld
@@ -55,3 +55,8 @@ class AccountUpdateView(UpdateView):
     # reverse는 function형 view에서 사용
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/update.html'
+
+class AccountDeleteView(DeleteView):
+    model = User
+    success_url = reverse_lazy('accountapp:login')
+    template_name = 'accountapp/delete.html'
